6358fff8ed4444bed490a633ee4aaf4605e0cabd
Rahul Patel <rp3313113@gmail.com>
2026-06-25 22:27:26 +0530

    Update face_analyzer.py

--- face_analyzer.py
+++ face_analyzer.py
@@ -2,8 +2,7 @@ import cv2
 import numpy as np
 from PIL import Image
 import mediapipe as mp
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+
 
 # Load cascades
 _face_cascade      = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
